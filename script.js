85e9c5f62676736f0694e2ea2547df486b8057d5
Anurag Das <anurag.das23.ad@gmail.com>
2020-11-13 20:35:52 -0500

    Using local storage to store selected seats and movie data

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 function updateSelectedCount() {
 const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
 const  selectedSeatsCount = selectedSeats.length;
-console.log(selectedSeatsCount);
 
 count.innerText = selectedSeatsCount;
 total.innerText = selectedSeatsCount * ticketPrice;
@@ -18,6 +27,7 @@ total.innerText = selectedSeatsCount * ticketPrice;
 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
